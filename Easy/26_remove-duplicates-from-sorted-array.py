f829270d016eb7cc78f9a0d2d4fdb6903d36c11e
Sandra Weinstock <238076497+sandra-weinstock@users.noreply.github.com>
2026-03-19 22:10:29 -0400

    Adding time and space complexity to 26

--- Easy/26_remove-duplicates-from-sorted-array.py
+++ Easy/26_remove-duplicates-from-sorted-array.py
@@ -2,13 +2,17 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         l = r = 0
 
-        while r < len(nums):
-            nums[l] = nums[r]
+        while r < len(nums): # O(n) or O(1) time
+            nums[l] = nums[r] # O(1) space
             r += 1
 
-            while r < len(nums) and nums[r] == nums[l]:
+            while r < len(nums) and nums[r] == nums[l]: # O(1) or O(n) time
                 r += 1
 
             l += 1
 
         return l
+
+        # n = len(nums)
+        # Time Complexity: O(n) + O(1) ≈ O(n)
+        # Space Complexity: O(1)
