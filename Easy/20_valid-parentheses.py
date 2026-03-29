9cddc9c8d7a2774d17418a3ec8fa4c6662e64af1
Sandra Weinstock <238076497+sandra-weinstock@users.noreply.github.com>
2026-03-29 19:09:09 -0400

    Add Valid Parentheses and Baseball Game solns with space and time complexity

--- Easy/20_valid-parentheses.py
+++ Easy/20_valid-parentheses.py
@@ -1,1 +1,23 @@
-hello world
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        matching = {"(":")", "[":"]", "{":"}"}
+
+        for c in s: # O(n) time
+            if c in matching: # O(1) time
+                stack.append(c) # O(n) space
+
+            elif len(stack) == 0: # length is member function of stack, O(1) time
+                return False
+
+            elif matching[stack.pop()] != c: # O(2) time
+                return False
+        
+        if len(stack) == 0:
+            return True
+        
+        return False
+
+# n = len(s)
+# Space Complexity: O(n)
+# Time Complexity: O(n)
--- Easy/682_baseball-game.py
+++ Easy/682_baseball-game.py
@@ -1,1 +1,27 @@
-helloworld
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record = [] # O(n) space
+        total = 0
+
+        for op in operations: # O(n) time
+            if op == "+":
+                new = record[-2] + record[-1] # 2 ops ... can't pop more than my list. at most n ops per n elts
+                record.append(new)
+            
+            elif op == "C":
+                record.pop()
+
+            elif op == "D":
+                new = 2 * record[-1]
+                record.append(new)
+            else:
+                record.append(int(op))
+        
+        for n in record: # O(n) time
+            total += n
+
+        return total
+
+# n = len(operations)
+# Space Complexity: O(n)
+# Time Complexity: O(2n)
