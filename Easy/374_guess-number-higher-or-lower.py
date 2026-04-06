5106b524bc44484bec4e5e169e820b39cc82102e
Sandra Weinstock <238076497+sandra-weinstock@users.noreply.github.com>
2026-04-06 12:35:48 -0400

    Add time and space complexity to 374.

--- Easy/374_guess-number-higher-or-lower.py
+++ Easy/374_guess-number-higher-or-lower.py
@@ -1,1 +1,19 @@
-helloworld
+class Solution:
+    def guessNumber(self, n: int) -> int:
+
+        r = n
+        l = 1
+
+        while True:
+
+            k = (l + r) // 2
+
+            if guess(k) == -1:
+                r = k - 1           # prevent repeated comparisions & infinite loop
+            elif guess(k) == 1:
+                l = k + 1           # same as above
+            else:
+                return k
+
+# O(logn) time. Happens when target num k is either 1 or n.
+# O(1) space
